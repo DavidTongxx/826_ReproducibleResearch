44c29f2aa377f7abb166205eeb282d33c2fbc8f7
David <xingxing@wisc.edu>
2016-04-24 13:39:10 -0500

    version0.0.1

--- backup/RegressionLine/R/Regressionline_function.R
+++ backup/RegressionLine/R/Regressionline_function.R
@@ -5,12 +5,16 @@
 #' @param numb How many points you want to generate? Defaults 100
 #' @param mu_x The mean of normal distribution that generates x Defaults 10
 #' @param mu_y The mean of normal distribution that generates y Defaults 20
-#' @return a picture
-#' @import ggplot2
+#' @return None
 #' @export
 #' @examples 
 #' Regressionline_function()
 Regressionline_function <- function(numb=100,mu_x=10,mu_y=20){
+  if (!requireNamespace("ggplot2", quietly = TRUE)) {
+    stop("ggplot2 needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  library(ggplot2)
   mydata<- data.frame( x=rnorm(n=numb,mean=mu_x,sd=10),
                        y=rnorm(n=numb,mean=mu_y,sd=10))
   ggplot(mydata, aes(x=x,y=y)) +geom_point()+geom_smooth(method = "lm",se=FALSE)
---
+++ buildPackage.R
@@ -0,0 +1,14 @@
+#826 R-package
+library(devtools)
+library(roxygen2)
+
+
+
+setwd("~/GitHub/826_ReproducibleResearch/RegressionLine")
+
+devtools::document()
+
+create("RegressionLine")
+
+
+
