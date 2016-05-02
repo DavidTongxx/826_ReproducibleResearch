2234ba130d6becc2feae8a3440b291ad44a0c475
Hello David <xingxing@wisc.edu>
2016-05-02 08:14:52 -0500

    Update test-gettingmean.R

--- RegressionLine/tests/testthat/test-gettingmean.R
+++ RegressionLine/tests/testthat/test-gettingmean.R
@@ -1,6 +1,4 @@
 context("getting mean")
-# This test function comes from broman/tests/testthat/test-runningmean.R
-
 test_that("getting mean stops when it should", {
   expect_equal( gettingmean(c(1,2)),1.5 )
   expect_equal( gettingmean(c(1,2,3,NA)) ,2)
